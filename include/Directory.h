bbc964e3bb5ffb264f8716df3249bc0999b57141
Rikarnto Bariampa <richard1996ba@gmail.com>
2021-12-31 15:38:34 +0200

    v0.2.2

---
+++ include/Directory.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+const char* AMPLE_IO_Directory_GetHomeDir();
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/Directory.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "AMPLE.h"
+
+#if defined(__LINUX__) || defined(__APPLE__)
+    #include <pwd.h>
+    #include <sys/types.h>
+    #include <unistd.h>
+#endif
+
+#include "Directory.h"
+
+const char* AMPLE_IO_Directory_GetHomeDir()
+{
+#if defined(__WINDOWS__)
+    char* returny;
+    size_t reqSize;
+    getenv_s(&reqSize, NULL, 0, "USERPROFILE");
+    returny = (char*)malloc(reqSize * sizeof(char));
+    getenv_s(&reqSize, returny, reqSize, "USERPROFILE");
+    return returny;
+#elif defined(__LINUX__) || defined(__APPLE__)
+    struct passwd* pw = getpwuid(getuid());
+    return pw->pw_dir;
+#endif
+}
